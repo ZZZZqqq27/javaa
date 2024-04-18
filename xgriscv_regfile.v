044029c3c0a4853a9378b92fbf869c2f599b961c
张智清 <zzqzzqzqzzq@163.com>
2024-04-18 13:02:16 +0800

    rechange

--- xgriscv_regfile.v
+++ xgriscv_regfile.v
@@ -8,36 +8,38 @@
 // The regfile module implements the core's general purpose registers file.
 //
 // ====================================================================
-
 `include "xgriscv_defines.v"
 
 module regfile(
   input                      	clk,
-  input  [`RFIDX_WIDTH-1:0]  	readaddress1, readaddress2,
-  output [`XLEN-1:0]          readdate1, readdata2,
+  input  [`RFIDX_WIDTH-1:0]  	ra1, ra2,
+  output [`XLEN-1:0]          rd1, rd2,
 
-  input                      	write, 
-  input  [`RFIDX_WIDTH-1:0]  	writeaddress,
-  input  [`XLEN-1:0]          writedata,
+  input                      	we3, 
+  input  [`RFIDX_WIDTH-1:0]  	wa3,
+  input  [`XLEN-1:0]          wd3,
   
   input  [`ADDR_SIZE-1:0] 	   pc
   );
 
-  reg [`XLEN-1:0] rf[`RFREG_NUM-1:0];//寄存器的大小已经define好了
+  reg [`XLEN-1:0] rf[`RFREG_NUM-1:0];
 
- 
+  // three ported register file
+  // read two ports combinationally
+  // write third port on falling edge of clock
+  // register 0 hardwired to 0
 
   always @(negedge clk)
-    if (write && writeaddress3!=0)//0寄存器要一直留着0
+    if (we3 && wa3!=0)
       begin
-        rf[writeaddress] <= writedata;
+        rf[wa3] <= wd3;
         // DO NOT CHANGE THIS display LINE!!!
         // 不要修改下面这行display语句！！！
         /**********************************************************************/
-        $display("pc = %h: x%d = %h", pc, writeaddress, writedata);
+        $display("pc = %h: x%d = %h", pc, wa3, wd3);
         /**********************************************************************/
       end
 
-  assign readdata1 = (readaddress1 != 0) ? rf[readaddress1] : 0;
-  assign readdata2 = (readaddress2 != 0) ? rf[readaddress2] : 0;
+  assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
+  assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
 endmodule
